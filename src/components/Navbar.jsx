6e2905e391473afef15d0158f34eef355a71dbec
StephenJBall <stephenjb94@gmail.com>
2024-02-19 15:47:31 +0000

    project setup and navbar created

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,43 @@
+import React, { useState, setState } from "react";
+import { Link } from "react-router-dom";
+
+function Navbar() {
+  const [click, setClick] = useState(false);
+
+  const handleClick = () => setClick(!click);
+  const closeMobileMenu = () => setClick(false);
+
+  return (
+    <div>
+      <nav className="navbar">
+        <div className="navbar-container">
+          <Link to="/" className="navbar-logo">
+            <i className="fa fa-home" aria-hidden="true"></i>
+          </Link>
+          <div className="nav-icon" onClick={handleClick}>
+            <i className={click ? "fas fa-times" : "fas fa-bars"}></i>
+          </div>
+          <ul className={click ? "nav-menu-active" : "nav-menu"}>
+            <li className="nav-item">
+              <Link className="nav-links" onClick={closeMobileMenu}>
+                Home
+              </Link>
+            </li>
+            <li className="nav-item">
+              <Link className="nav-links" onClick={closeMobileMenu}>
+                Projects
+              </Link>
+            </li>
+            <li className="nav-item">
+              <Link className="nav-links" onClick={closeMobileMenu}>
+                Contact
+              </Link>
+            </li>
+          </ul>
+        </div>
+      </nav>
+    </div>
+  );
+}
+
+export default Navbar;
