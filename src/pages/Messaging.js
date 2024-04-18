cec8232902e860ae3aeb0bf2b72530f05cc424ce
StephenJBall <stephenjb94@gmail.com>
2024-04-18 10:17:49 +0100

    Instant messaging section done

--- src/pages/Messaging.js
+++ src/pages/Messaging.js
@@ -1,9 +1,27 @@
-import React from 'react'
+import React from "react";
 
 function Messaging() {
   return (
-    <div>Hello, World!</div>
-  )
+    <div className="project-description-container">
+      <h2>Instant Messaging Application</h2>
+      <p>
+        A fully-responsive instant-messaging application built using Stream.
+      </p>
+      <p>
+        It offers Direct Messaging, Group Messaging, Channels, emojis,
+        reactions, GIF Support, the ablility to edit and delete messages, as
+        well as create, edit, moderate, and delete channels for specific groups
+        or topics.
+      </p>
+      <h2>Want to have a look at the code?</h2>
+      <a
+        href="https://github.com/StephenJBall/instant-messaging-application"
+        target="_blank"
+      >
+        <p>Git Hub Repository</p>
+      </a>
+    </div>
+  );
 }
 
-export default Messaging
+export default Messaging;
--- src/pages/reddit.js
+++ src/pages/reddit.js
@@ -1,9 +1,27 @@
-import React from 'react'
+import React from "react";
 
 function reddit() {
   return (
-    <div>Hello, World!</div>
-  )
+    <div className="project-description-container">
+      <h2>Reddit Clone</h2>
+      <p>
+        This is a simple Reddit Clone project built using React, Typescript,
+        Tailwind, SQL, and GraphQL.
+      </p>
+      <p>
+        Users need to sign in through their own Reddit accounts to gain full
+        functionality to the application.
+      </p>
+      <p>
+        Once signed in users can; create posts, comment on posts, upvote,
+        downvote, visit subreddits, and create new subreddits.
+      </p>
+      <h2>Want to have a look at the code?</h2>
+      <a href="https://github.com/StephenJBall/reddit-clone" target="_blank">
+        <p>Git Hub Repository</p>
+      </a>
+    </div>
+  );
 }
 
-export default reddit
+export default reddit;
