119c478d062fea15df0e84a55ee167082328d76a
StephenJBall <stephenjb94@gmail.com>
2024-04-18 09:21:19 +0100

    Portfolio section finished

--- src/pages/PortfolioWebsite.js
+++ src/pages/PortfolioWebsite.js
@@ -1,8 +1,14 @@
 import React from 'react'
+import './ProjectDescription.css'
 
 function PortfolioWebsite() {
   return (
-    <div>Hello, World!</div>
+    <div className='project-description-container'>
+      <h2>Portfolio Website</h2>
+      <p>A React-based website that I designed to host my projects, showcase my software engineering skills, and provide more context around the type of person I am.</p>
+      <h2>Want to have a look at the code?</h2>
+      <a href='https://github.com/StephenJBall/portfolio-website' target='_blank'><p>Git Hub Repository</p></a>
+    </div>
   )
 }
 
